48bcaa8482593cb7b3e68d49b1173ffed5ae3f1d
Flor87Posta <florenciapostacchini@gmail.com>
2023-04-04 13:30:21 -0300

    Task2 Created Accounts

---
+++ HomebankingApplication.java
@@ -0,0 +1,65 @@
+package com.mindhub.homebanking;
+
+import com.mindhub.homebanking.models.Account;
+import com.mindhub.homebanking.repositories.AccountRepository;
+import com.mindhub.homebanking.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
+import com.mindhub.homebanking.models.Client;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+
+public class HomebankingApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HomebankingApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(ClientRepository  clientRepository, AccountRepository  accountRepository) {
+		return (args) -> {
+
+			Client client1 = new Client("Melba", "Morel", "melba@mindhub.com");
+			Client client2 = new Client("Lucia", "Lopez", "lucilopez@mindhub.com");
+			Client client3 = new Client("Clara", "Gonzalez", "claragonzalez@mindhub.com");
+
+
+			Account account1 = new Account("VIN001", LocalDateTime.now(), 5000);
+			Account account2 = new Account("VIN002", LocalDateTime.now().plusDays(1), 7500);
+			Account account3 = new Account("VIN003", LocalDateTime.now(), 10000);
+			Account account4 = new Account("VIN004", LocalDateTime.now(), 15000);
+
+
+			clientRepository.save(client1);
+			clientRepository.save(client2);
+			clientRepository.save(client3);
+
+			client1.addAccount(account1);
+			client1.addAccount(account2);
+			client2.addAccount(account3);
+			client3.addAccount(account4);
+
+			accountRepository.save(account1);
+			accountRepository.save(account2);
+			accountRepository.save(account3);
+			accountRepository.save(account4);
+
+			clientRepository.save(client1);
+			clientRepository.save(client2);
+			clientRepository.save(client3);
+
+
+
+		};
+
+	}
+}
+
+
+
+
---
+++ dtos/ClientDTO.java
@@ -0,0 +1,77 @@
+package com.mindhub.homebanking.dtos;
+
+
+
+import com.mindhub.homebanking.models.Client;
+
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ClientDTO {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    public Set<AccountDTO> accounts;
+
+
+    public ClientDTO(){}
+
+    public ClientDTO(Client client) {
+
+        this.id = client.getId();
+
+        this.firstName = client.getFirstName();
+
+        this.lastName = client.getLastName();
+
+        this.email = client.getEmail();
+
+        this.accounts = client.getAccountSet().stream().map(account -> new AccountDTO(account)).collect(Collectors.toSet());
+    }
+
+        public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    } // no hace falta
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientDTO{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
+
