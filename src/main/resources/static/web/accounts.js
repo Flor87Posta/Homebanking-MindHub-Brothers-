5cf9c47d57900ddcb3816f47025b2373028b3f6c
FLORENCIA POSTACCHINI <florenciapostacchini@gmail.com>
2023-04-13 18:18:42 -0300

    Task4: en esta task se añade el modulo de préstamos

---
+++ src/main/resources/static/web/accounts.js
@@ -0,0 +1,67 @@
+
+const {createApp} = Vue
+
+
+const app = createApp ({
+
+    data(){
+        return {
+            firstName : '',
+            lastName : '',
+            accounts: [],
+            clients: [],
+            loans: [],
+
+            
+
+        }
+    },
+    created(){
+    //         // Hacer una petición a través de una función loadData
+            this.loadData();
+    
+
+            },
+
+        methods: {
+
+        loadData(){
+                axios.get("http://localhost:8080/api/clients/1")
+                    .then(response => {
+                        this.clients=response.data;
+                        console.log(this.clients)
+                        this.accounts=this.clients.accounts
+                        this.loans=this.clients.loans
+
+
+
+                    })
+                    .catch(err => console.log( err ));
+                },
+
+        addClient(){
+            this.postClient();
+        },
+
+        postClient(){
+                axios.post("http://localhost:8080/api/clients/1", {
+                    firstName: this.firstName,
+                    lastName: this.lastName,
+                    email: this.email,
+                    accounts: this.accounts,
+
+                    
+                })
+                .then(function (response) {
+                    this.loadData();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            },
+
+            },
+
+})
+
+app.mount('#app')
